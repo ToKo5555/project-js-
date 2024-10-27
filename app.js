3bf43102d9142c383f36f956f25f8ebfccd2d1cc
shushiashvili <shushiashvili.t@gmail.com>
2024-10-28 02:48:30 +0400

    js project

---
+++ app.js
@@ -0,0 +1,157 @@
+let navbar = document.getElementById('navbar');
+let ul = document.getElementById("myul")
+let section = document.getElementById("mysct")
+let section2 = document.getElementById("fullsection")
+let li = document.getElementById("all")
+let inputrange = document.getElementById("range")
+let buttonreset = document.getElementsByClassName("reset")
+let nuts = document.getElementById("nuts")
+let vegan = document.getElementById("vegan")
+let filterBtn = document.getElementById("filterBtn")
+let label = document.getElementById("spiciness-label");
+let card = document.getElementById("filtercard")
+let reset = document.getElementsByClassName("reset")
+let spicinessRange = document.getElementById("spicinessRange");
+let spicinessValue = document.getElementById("spicinessValue");
+let filterReset = document.getElementById("filterReset");
+let filterApply = document.getElementById("filterApply");
+
+window.onscroll = function () {
+  let navbar = document.getElementById("navbar");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+};
+
+
+
+   fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll").then(pasuxi2 => pasuxi2.json()).then(data2 =>{
+     data2.forEach(item2 => myul.innerHTML +=showNavbar(item2))
+    
+    })
+   function showNavbar(item2) {
+     return `<li onclick="showCardsByCategory(${item2.id})">${item2.name}</li>`
+   }
+   function showCardsByCategory(id) {
+    card.innerHTML = ""
+   fetch(`https://restaurant.stepprojects.ge/api/Categories/GetCategory/${id}`).
+   then(pasuxi => pasuxi.json())
+   .then(finalData => finalData.products.forEach(list => card.innerHTML += showcards(list)
+       ));
+      }
+
+      
+      fetch("https://restaurant.stepprojects.ge/api/Products/GetAll")
+.then(pasuxi => pasuxi.json ())
+.then (data =>{
+     data.forEach(list => card.innerHTML += showcards(list))
+    
+});
+function showcards(list){
+return `
+           <div class= "allproductcard"> <img src="${list.image}" alt="">
+            <h3>${list.name}</h3>
+            <p>spiciness:${list.spiciness}</p>
+            <div class = "inputradio">
+                <i class=></i>
+                <i class="${list.nuts ?"fa-regular fa-circle-check" : "fa-regular fa-circle"}"></i>
+                <label for="">nuts</label>
+                <i class="${list.vegeterian ?"fa-regular fa-circle-check" : "fa-regular fa-circle"}"></i>
+                <label for="">Vegeterian</label>
+            </div>
+            <div class = "cardbottom">
+                <h2>$${list.price}</h2>
+                <button onclick = "addcart(${list.id},${list.price})"class = "borderblack" type="submit">Add To Cart</button>
+            </div></div>`
+            
+  }
+ 
+  
+
+ function addcart(prodId,prodPrice){
+  console.log(prodPrice);
+  let cartinfo = {
+    quantity: 1,
+    price: prodPrice,
+    productId:prodId,
+   
+  }
+  fetch("https://restaurant.stepprojects.ge/api/Baskets/AddToBasket",{
+    method: "POST",
+    headers: {
+      accept : 'text/plain',
+      'Content-Type': 'application/json',
+       
+    },
+    body:JSON.stringify(cartinfo)
+  }).then(res => res.text()).then(() => alert("წარმატებით დაემატა"))
+ }
+
+ 
+
+
+
+let spiciness = "";
+filterApply.addEventListener("click", function (e) {
+  e.preventDefault();
+  spicinessRange.value > "-1"
+    ? (spiciness = spicinessRange.value)
+    : (spiciness = "");
+  nuts.checked ? (nuts.value = true) : "";
+  vegan.checked ? (vegan.value = true) : "";
+  !nuts.checked ? (nuts.value = "") : (nuts.value = false);
+  !vegan.checked ? (vegan.value = "") : (vegan.value = true);
+ 
+  fetch(
+    `https://restaurant.stepprojects.ge/api/Products/GetFiltered?vegeterian=${vegan.value}&nuts=${nuts.value}&spiciness=${spiciness}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+     card.innerHTML = "";
+      data.forEach((list) => (card.innerHTML += showcards(list)));
+      });
+});
+ 
+filterReset.addEventListener("click", function () {
+  fetch("https://restaurant.stepprojects.ge/api/Products/GetAll")
+    .then((response) => response.json())
+    .then((data) => {
+      card.innerHTML = "";
+      data.forEach((list) => (card.innerHTML += showcards(list)));
+      });
+  spicinessRange.value = "-1";
+  spicinessValue.innerText =
+    spicinessRange > -1 ? spicinessRange : "Not Chosen";
+  nuts.checked ? (nuts.checked = false) : "";
+  vegan.checked ? (vegan.checked = false) : "";
+});
+ 
+spicinessRange.addEventListener("input", function () {
+  let spicinessRangeVal = spicinessRange.value;
+  spicinessValue.innerText =
+    spicinessRangeVal > -1 ? spicinessRangeVal : "Not Chosen";
+});
+
+
+
+fetch("https://restaurant.stepprojects.ge/api/Products/GetAll")
+.then(pasuxi => pasuxi.json ())
+.then (data =>{
+    console.log(data);
+    showcards(data)
+    
+});
+
+function toggleSlide() {
+  const slide = document.getElementById('slide');
+  if (slide.classList.contains('active')) {
+      slide.classList.remove('active');
+  } else {
+      slide.classList.add('active');
+  }
+}
+
+
+
---
+++ cart.js
@@ -0,0 +1,118 @@
+let table = document.querySelector("table")
+let navbar = document.querySelector("nav");
+let tBody = document.getElementById("rows");
+let  table2 = document.getElementById("table2")
+let sum = document.getElementById("sum")
+window.onscroll = function () {
+    let navbar = document.getElementById("navbar");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  };
+async function getCart() {
+    let cartList = await fetch("https://restaurant.stepprojects.ge/api/Baskets/GetAll")
+    return cartList.json()
+}
+getCart().then(data => data.forEach(item => tBody.innerHTML += cartRows(item)))
+
+
+
+
+
+function cartRows(item){
+  
+    return `   
+             <tr class = "background">
+                <td class = "mytd" onclick><i class="fa-solid fa-x" onclick = "deleteX(${item.product.id})"></i> <i class="fa-solid fa-pencil"></i></td>
+                <td><img src= "${item.product.image}"</td>
+                <td class = "mainproductname">${item.product.name}</td>
+                <td><i class="fa-solid fa-circle-plus" onclick = "miumate (${item.price},${item.quantity},${item.product.id})"></i>${item.quantity}<i class="fa-solid fa-circle-minus"onclick = "daaminuse (${item.price},${item.quantity},${item.product.id})"></i></td>
+                <td>$${item.price}</td>
+                <td>$${item.price * item.quantity}</td>
+            </tr> `;
+          
+}
+async function async() {
+    let listItems = fetch(
+      "https://restaurant.stepprojects.ge/api/Baskets/GetAll"
+    );
+    return (await listItems).json();
+  }
+  
+  function deleteX(id) {
+    fetch(`https://restaurant.stepprojects.ge/api/Baskets/DeleteProduct/${id}`, {
+      method: "DELETE",
+      headers: {
+        accept: "*/*",
+      },
+    })
+      .then((data) => data.text())
+      .then((deleteData) => {
+        tBody.innerHTML = ""
+        async().then((data) =>
+          data.forEach((item) => tBody.innerHTML += cartRows(item)));
+      });
+  }
+function miumate(cardPrice,cardQuantity,cardId){
+    cardQuantity ++
+    let dasaafteibeli = {
+     quantity: cardQuantity,
+     price: cardPrice,
+     productId: cardId,
+    }
+fetch("https://restaurant.stepprojects.ge/api/Baskets/UpdateBasket",{
+    method:"PUT",
+    headers:{
+        accept:'*/*',
+       'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dasaafteibeli)
+    }).then(res => res.text()).then(data =>{
+     tBody.innerHTML =""
+    getCart().then(data => data.forEach(item => 
+        tBody.innerHTML += cartRows(item)))
+})
+
+}
+function daaminuse(cardPrice,cardQuantity,cardId){
+    cardQuantity --;
+    let dasaafteibeli = {
+     quantity: cardQuantity,
+     price: cardPrice,
+     productId: cardId,
+    }
+fetch("https://restaurant.stepprojects.ge/api/Baskets/UpdateBasket",{
+    method:"PUT",
+    headers:{
+        accept:'*/*',
+       'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dasaafteibeli)
+}).then(res => res.text()).then(data =>{
+   tBody.innerHTML = ""
+    getCart().then(data => data.forEach(item => tBody.innerHTML += cartRows(item)))
+})
+
+}
+fetch("https://restaurant.stepprojects.ge/api/Baskets/GetAll")
+.then(res => res.json())
+.then(data => {
+  let allPrice = data.map((item) => item.quantity * item.price);
+  let sumPrice = allPrice.reduce((prev,current) => {
+    return prev + current
+  })
+   sum.innerText +=  sumPrice + "$"
+  
+   
+})
+function toggleSlide() {
+  const slide = document.getElementById('slide');
+  if (slide.classList.contains('active')) {
+      slide.classList.remove('active');
+  } else {
+      slide.classList.add('active');
+  }
+}
+
